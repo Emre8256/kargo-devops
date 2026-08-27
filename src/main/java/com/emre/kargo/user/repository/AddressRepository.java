c93c5ef9a2fddcacce67d722275a5e541c2219d7
Yunus Emre Öztürk <42oyunus42@gmail.com>
2026-08-27 12:04:10 +0300

    last

--- src/main/java/com/emre/kargo/user/repository/AddressRepository.java
+++ src/main/java/com/emre/kargo/user/repository/AddressRepository.java
@@ -7,6 +7,7 @@ import java.util.List;
 import java.util.Optional;
 
 public interface AddressRepository extends JpaRepository<Address,Long> {
+    
     List<Address> findByUserId(Long userId);
     Optional<Address> findByIdAndUserId(Long id, Long userId);
 }
